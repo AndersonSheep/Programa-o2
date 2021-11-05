99aad59dbd0634fb5e0a471e5c358ad52fe85b3b
AndersonSheep <61760830+AndersonSheep@users.noreply.github.com>
2021-11-05 03:21:38 +0000

    Todas exigencias atendidas

--- Projeto1/Main.java
+++ Projeto1/Main.java
@@ -1,5 +1,12 @@
 class Main {
   public static void main(String[] args) {
     System.out.println("Hello world!");
+    Point p = new Point(10,10);
+    p.setX(150);
+    System.out.println(p.getX());
+    p.moveBy(308, 150);
+        System.out.println(p.getX());
+
+
   }
 }
--- Projeto1/Point.java
+++ Projeto1/Point.java
@@ -1,22 +1,47 @@
+/*
+(1) Implemente um construtor para que seja possivel criar uma instancia de Point da seguinte forma: Point p = new Point(10, 10);
+(2) Verifique questao de "data hiding" para assegurar um encapsulamento onde os fields nao sejam visiveis fora do modulo
+(3) Existe um contrato [invariante de classe] que exige que as coordenadas representadas pela classe Point sejam positivas e menores ou iguais a 400, ou seja, (x >=0 && x <= 400) && (y >=0 && y <= 400). Assegure para que a classe mantenha essa integridade ao ser manipulada por codigo clientes.
+
+(4) Ao finalizar, favor finalizar essa task e submeter a sua versao da classe Point final.
+***OBS: Pode submeter um arquivo .txt com o codigo da classe ou apenas o texto da mesma.
+*/
+
 public class Point {
-    private int x = 0, y = 0;
 
-    public int getX() { 
-      return x; 
-    }
-    public int getY() { 
-      return y; 
-    }
+  //Colocando os fields como private para garantir o "data hiding" 
+  private int x = 0, y = 0;
+  
+  //Construtor
+  public Point(int coordenadaX, int coordenadaY){
+    this.x=coordenadaX;
+    this.y=coordenadaY;
+  }
 
-    public void setX(int x) { 
-        this.x = x; 
-    }
-    public void setY(int y) { 
-        this.y = y; 
-    }
+  public int getX() { 
+    return x; 
+  }
+  public int getY() { 
+    return y; 
+  }
 
-    public void moveBy(int dx, int dy) {
-        setX(getX() + dx);
-        setY(getY() + dy);
-    }
+  public void setX(int x) { 
+    //Garantindo que sejam positivas e menores ou iguais a 400
+    if(x >=0 && x <= 400)
+      this.x = x; 
+    else
+      System.out.println("O valor deve ser positivo e menor ou igual que 400");
+  }
+  public void setY(int y) { 
+    //Garantindo que sejam positivas e menores ou iguais a 400
+    if (y >=0 && y <= 400)
+      this.y = y; 
+    else
+      System.out.println("O valor deve ser positivo e menor ou igual que 400");
+  }
+
+  public void moveBy(int dx, int dy) {
+      setX(getX() + dx);
+      setY(getY() + dy);
+  }
 }
